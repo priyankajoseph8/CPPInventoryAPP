46aa71b7efb6076d2d9f3e721125af71504647b4
EC2 Default User <ec2-user@ip-172-31-36-22.eu-west-3.compute.internal>
2023-04-25 18:40:01 +0000

    new changes

--- core/settings.py
+++ core/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'                     # bootstrap template crispy-form uses
